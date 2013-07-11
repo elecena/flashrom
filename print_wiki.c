elecena/flashrom

commit b587aebb2d66f242d8609552a39ed52a56f44c6e

Introduce enum test_state.
Previously boards in the wiki were tagged either as working or as known
bad. But we added support to various boards via board enables that were
then never tested because the owners have not reported back. This can
now be tagged with NT and is shown appropriately.

Also, the underlying data structure indicating state was converted from
macros to an enum while preserving original integer values and is used
for programmers and chipsets too.

Signed-off-by: Stefan Tauner <stefan.tauner@student.tuwien.ac.at>
Acked-by: Stefan Tauner <stefan.tauner@student.tuwien.ac.at>

git-svn-id: svn://coreboot.org/flashrom/trunk@1555 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

print_wiki.c
@@ -129,23 +129,27 @@ static void print_supported_chipsets_wiki(int cols)
 static void wiki_helper(const char *devicetype, int cols,
 			const struct board_info boards[])
 {
-	int i, j, k = 0, boardcount_good = 0, boardcount_bad = 0, color = 1;
-	int num_notes = 0;
+	int i, j, k;
+	unsigned int boardcount_good = 0, boardcount_bad = 0, boardcount_nt = 0;
+	int num_notes = 0, color = 1;
 	char *notes = calloc(1, 1);
 	char tmp[900 + 1];
 	const struct board_match *b = board_matches;
 
 	for (i = 0; boards[i].vendor != NULL; i++) {
-		if (boards[i].working)
+		if (boards[i].working == OK)
 			boardcount_good++;
+		else if (boards[i].working == NT)
+			boardcount_nt++;
 		else
 			boardcount_bad++;
 	}
 
-	printf("\n\nTotal amount of supported %s: '''%d'''. "
-	       "Not yet supported (i.e., known-bad): '''%d'''.\n\n"
+	printf("\n\nTotal amount of known good boards %s: '''%d'''; "
+	       "Untested (e.g. user vanished before testing new code): '''%d'''; "
+	       "Not yet supported (i.e. known-bad): '''%d'''.\n\n"
 	       "{| border=\"0\" valign=\"top\"\n| valign=\"top\"|\n\n%s",
-	       devicetype, boardcount_good, boardcount_bad, board_th);
+	       devicetype, boardcount_good, boardcount_nt, boardcount_bad, board_th);
 
 	for (i = 0, j = 0; boards[i].vendor != NULL; i++, j++) {
 
@@ -171,7 +175,8 @@ static void wiki_helper(const char *devicetype, int cols,
 		       b[k].lb_vendor ? b[k].lb_vendor : "",
 		       b[k].lb_vendor ? ":" : "",
 		       b[k].lb_vendor ? b[k].lb_part : "",
-		       (boards[i].working) ? "OK" : "No");
+		       (boards[i].working == OK) ? "OK" :
+		       (boards[i].working == NT) ? "?3" : "No");
 
 		if (boards[i].note) {
 			printf("<sup>%d</sup>\n", num_notes + 1);

flash.h
@@ -253,8 +253,11 @@ int doit(struct flashctx *flash, int force, const char *filename, int read_it, i
 int read_buf_from_file(unsigned char *buf, unsigned long size, const char *filename);
 int write_buf_to_file(unsigned char *buf, unsigned long size, const char *filename);
 
-#define OK 0
-#define NT 1    /* Not tested */
+enum test_state {
+	OK = 0,
+	NT = 1,	/* Not tested */
+	BAD
+};
 
 /* Something happened that shouldn't happen, but we can go on. */
 #define ERROR_NONFATAL 0x100

programmer.h
@@ -148,7 +148,7 @@ struct pci_dev;
 struct penable {
 	uint16_t vendor_id;
 	uint16_t device_id;
-	int status; /* OK=0 and NT=1 are defines only. Beware! */
+	const enum test_state status;
 	const char *vendor_name;
 	const char *device_name;
 	int (*doit) (struct pci_dev *dev, const char *name);
@@ -190,7 +190,7 @@ struct board_match {
 	const char *board_name;
 
 	int max_rom_decode_parallel;
-	int status;
+	const enum test_state status;
 	int (*enable) (void); /* May be NULL. */
 };
 
@@ -199,7 +199,7 @@ extern const struct board_match board_matches[];
 struct board_info {
 	const char *vendor;
 	const char *name;
-	const int working;
+	const enum test_state working;
 #ifdef CONFIG_PRINT_WIKI
 	const char *url;
 	const char *note;
@@ -224,7 +224,7 @@ extern struct pci_dev *pcidev_dev;
 struct pcidev_status {
 	uint16_t vendor_id;
 	uint16_t device_id;
-	int status;
+	const enum test_state status;
 	const char *vendor_name;
 	const char *device_name;
 };
@@ -425,7 +425,7 @@ extern const struct pcidev_status ata_hpt[];
 struct usbdev_status {
 	uint16_t vendor_id;
 	uint16_t device_id;
-	int status;
+	const enum test_state status;
 	const char *vendor_name;
 	const char *device_name;
 };

commit 029be82211c1ad28d6a2ae9bd7f137805eeb1485

print_wiki: Fixes, explanation re. flash chips, links to footnotes.
 - Escape mainboard notes in the wiki output with <nowiki> (this became
   necessary because MediaWiki started to convert <mark@zl2tod.net> to <mark>).
 - Add explanations at the top of the flash chip table in the wiki.
 - Add links to the footnotes of mainboards and laptops similarly to
   what MediaWiki does: link from the reference to the footnote and
   vice versa too. Also, check for truncations of the footnotes due
   to the limited buffer.
 - Fix a typo introduced in r1555.

Signed-off-by: Stefan Tauner <stefan.tauner@student.tuwien.ac.at>
Acked-by: Stefan Tauner <stefan.tauner@student.tuwien.ac.at>

print_wiki.c
@@ -76,6 +76,18 @@ static const char chip_th[] = "\
 | Probe\n| Read\n| Erase\n| Write\n\
 | align=\"center\" | Min \n| align=\"center\" | Max\n\n";
 
+static const char chip_intro[] = "\
+\n== Supported flash chips ==\n\n\
+The list below contains all chips that have some kind of explicit support added to flashrom and their last \
+known test status. Newer SPI flash chips might work even without explicit support if they implement SFDP ([\
+http://www.jedec.org/standards-documents/docs/jesd216 Serial Flash Discoverable Parameters - JESD216]). \
+Flashrom will detect this automatically and inform you about it.\n\n\
+The names used below are designed to be as concise as possible and hence contain only the characters \
+describing properties that are relevant to flashrom. Irrelevant characters specify attributes flashrom can not \
+use or even detect by itself (e.g. the physical package) and have no effect on flashrom's operation. They are \
+replaced by dots ('.') functioning as wildcards (like in Regular Expressions) or are completely omitted at the \
+end of a name.\n";
+
 static const char programmer_th[] = "\
 ! align=\"left\" | Programmer\n\
 ! align=\"left\" | Vendor\n\
@@ -159,7 +171,7 @@ static void print_supported_boards_wiki_helper(const char *devicetype, int cols,
 	/* +1 to force the resulting number of columns to be < cols */
 	lines_per_col = boardcount / cols + ((boardcount%cols) > 0 ? 1 : 0);
 
-	printf("\n\nTotal amount of known good boards %s: '''%d'''; "
+	printf("\n\nTotal amount of known good %s: '''%d'''; "
 	       "Untested (e.g. user vanished before testing new code): '''%d'''; "
 	       "Not yet supported (i.e. known-bad): '''%d'''.\n\n"
 	       "{| border=\"0\" valign=\"top\"\n", devicetype, boardcount_good, boardcount_nt, boardcount_bad);
@@ -194,9 +206,17 @@ static void print_supported_boards_wiki_helper(const char *devicetype, int cols,
 		       (boards[i].working == NT) ? "?3" : "No");
 
 		if (boards[i].note) {
-			printf("<sup>%d</sup>\n", num_notes + 1);
-			snprintf(tmp, sizeof(tmp), "<sup>%d</sup> %s<br />\n",
-				 1 + num_notes++, boards[i].note);
+			num_notes++;
+			printf(" <span id=\"%s_ref%d\"><sup>[[#%s_note%d|%d]]</sup></span>\n",
+			       devicetype, num_notes, devicetype, num_notes, num_notes);
+			int ret = snprintf(tmp, sizeof(tmp),
+					   "<span id=\"%s_note%d\">%d. [[#%s_ref%d|&#x2191;]]</span>"
+					   " <nowiki>%s</nowiki><br />\n", devicetype, num_notes, num_notes,
+					   devicetype, num_notes, boards[i].note);
+			if (ret < 0 || ret >= sizeof(tmp)) {
+				fprintf(stderr, "Footnote text #%d of %s truncated (ret=%d, sizeof(tmp)=%zu)\n",
+					num_notes, devicetype, ret, sizeof(tmp));
+			}
 			notes = strcat_realloc(notes, tmp);
 		} else {
 			printf("\n");
@@ -248,7 +268,8 @@ static void print_supported_chips_wiki(int cols)
 	/* +1 to force the resulting number of columns to be < cols */
 	lines_per_col = chipcount / cols + ((chipcount%cols) > 0 ? 1 : 0);
 
-	printf("\n== Supported chips ==\n\nTotal amount of supported chips: '''%d'''\n\n"
+	printf("%s", chip_intro);
+	printf("\nTotal amount of supported chips: '''%d'''\n\n"
 	       "{| border=\"0\" valign=\"top\"\n", chipcount);
 
 	for (f = flashchips; f->name != NULL; f++) {